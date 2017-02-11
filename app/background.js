ac2d9ec56bac912e55d08a35196618072d475f93
sosuke-k <snoopies.drum@gmail.com>
2017-02-12 01:03:58 +0900

    pandlerのsvgを追加

--- app/background.js
+++
@@ -1,6 +0,0 @@
-// chrome.browserAction.onClicked.addListener(function (tab) {
-//   var url = "pandler-test1://" + tab.url.split("://")[1];
-//   alert(url);
-//   chrome.tabs.sendMessage(tab.id, tab.url)
-//   // chrome.tabs.update(tab.id, {url: url});
-// });
--- app/content_scripts.js
+++ app/content_scripts.js
@@ -1,26 +1,12 @@
-console.log('content_scripts');
-
 window.addEventListener('load', (function(){
-  console.log('content_scripts loaded');
-  // var a = document.querySelector('h1 strong a');
+  var img = document.createElement('img');
+  img.type = "image/svg+xml";
+  img.src = chrome.extension.getURL('images/pandler.svg');
+  img.style = "width: 32px; height: 32px;";
+  img.className = "v-align-middle";
   var a = document.createElement("a");
-  a.text = "pandler"
-  a.href = "pandler-test1://" + document.location.host + document.location.pathname;
-  a.className = "label v-align-middle";
-  var style = "";
-  style += "background: #CE93D8;"
-  style += "color: rgba(0,0,0,.87);"
-  style += "font-weight: 600;"
-  a.style = style;
-  console.log(a.href);
+  a.appendChild(img);
+  a.style = "margin-left: 12px; width: 32px; height: 32px;";
+  a.href = "pandler://" + document.location.host + document.location.pathname;
   document.querySelector('h1').appendChild(a);
 })(),false);
-
-// chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-//   var a = document.querySelector('h1 strong a');
-//   a.href = "pandler-test1://" + document.location.host + document.location.pathname;
-//   console.log('red');
-//   console.log(msg);
-//   console.log(document.location)
-//   document.body.style.backgroundColor = 'red';
-// });
